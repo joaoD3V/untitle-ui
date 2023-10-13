f0108854b09db3623a190bd65159019066fa58f6
Joao Silva <joao.devweb@gmail.com>
2023-10-13 11:51:24 -0300

    Add context to handle files on Input.Root and show image preview

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,7 +1,31 @@
-import { ComponentProps } from 'react';
+'use client';
+
+import { ChangeEvent, ComponentProps } from 'react';
+import { useFileInput } from './Root';
 
 type ControlProps = ComponentProps<'input'>;
 
-export function Control({ ...props }: ControlProps) {
-  return <input id="photo" type="file" className="sr-only" {...props} />;
+export function Control({ multiple = false, ...props }: ControlProps) {
+  const { id, onFilesSelected } = useFileInput();
+
+  function handleFilesSelected(e: ChangeEvent<HTMLInputElement>) {
+    if (!e.target.files?.length) {
+      return;
+    }
+
+    const files = Array.from(e.target.files); // Transforma a FileList em Array
+
+    onFilesSelected(files, multiple);
+  }
+
+  return (
+    <input
+      id={id}
+      type="file"
+      className="sr-only"
+      onChange={handleFilesSelected}
+      multiple={multiple}
+      {...props}
+    />
+  );
 }
--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,7 +1,42 @@
-import { ComponentProps } from 'react';
+'use client';
+
+import {
+  ComponentProps,
+  createContext,
+  useContext,
+  useId,
+  useState,
+} from 'react';
 
 type RootProps = ComponentProps<'div'>;
 
+type FileInputContextType = {
+  id: string;
+  files: File[];
+  onFilesSelected: (files: File[], multiple: boolean) => void;
+};
+
+const FileInputContext = createContext({} as FileInputContextType);
+
 export function Root({ ...props }: RootProps) {
-  return <div {...props} />;
+  const id = useId(); // Cria um id único e persiste esse id entre as renderizações do componente
+  const [files, setFiles] = useState<File[]>([]);
+
+  function handleSelectedFiles(files: File[], multiple: boolean) {
+    if (multiple) {
+      setFiles((prev) => [...prev, ...files]);
+    } else {
+      setFiles(files);
+    }
+  }
+
+  return (
+    <FileInputContext.Provider
+      value={{ id, files, onFilesSelected: handleSelectedFiles }}
+    >
+      <div {...props} />
+    </FileInputContext.Provider>
+  );
 }
+
+export const useFileInput = () => useContext(FileInputContext);
